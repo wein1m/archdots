84db0a6994ae550ddacbef1630bd568fc4e3e083
wein1m <weynn.sz@gmail.com>
2026-03-14 17:31:12 +0800

    added thefuck✌️

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ if status is-interactive
     # Starship custom prompt
     starship init fish | source
 
+    thefuck --alias | source
+
     # Direnv + Zoxide
     command -v direnv &> /dev/null && direnv hook fish | source
     command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
